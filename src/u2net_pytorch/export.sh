90ac877c3cf38f411a622071ec37ce7c7a8d2efc
viji2029@hellosivi.com <vijayapurani@hellosivi.com>
2023-09-06 11:28:21 +0530

    shell command to export

---
+++ src/u2net_pytorch/export.sh
@@ -0,0 +1,4 @@
+python3 run.py export --model_path "/home/vijayapurani/cosmos/repo/bgrem-base/src/u2net_pytorch/outputs/checkpoints/2023_09_05_22_55_50/u2net_train_loss_epoch=0000_train_loss=0.00.ckpt"\
+                      --out_path "/home/vijayapurani/cosmos/repo/bgrem-base/src/u2net_pytorch/outputs/checkpoints/onnxfiles./u2net_kg_export.onnx"
+
+
