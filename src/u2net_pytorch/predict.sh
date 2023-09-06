4925d045ab1087d2c95d40aec30afe242e5dcdb8
viji2029@hellosivi.com <vijayapurani@hellosivi.com>
2023-09-06 11:28:33 +0530

    shell command to predict

---
+++ src/u2net_pytorch/predict.sh
@@ -0,0 +1,3 @@
+python3 run.py predict  --model_path "/home/vijayapurani/cosmos/repo/bgrem-base/src/u2net_pytorch/outputs/checkpoints/onnxfiles/u2net_kg_export.onnx"\
+                        --input_image "/home/vijayapurani/cosmos/repo/bgrem-base/tests/SIVI-TEST/image/1.jpg" 
+                        
