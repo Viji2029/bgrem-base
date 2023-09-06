f0c8e42dbda457f946fd5c2e8baf18fdb609caed
viji2029@hellosivi.com <vijayapurani@hellosivi.com>
2023-09-06 13:10:15 +0530

    changed batch size

--- src/u2net_pytorch/train.sh
+++ src/u2net_pytorch/train.sh
@@ -1,1 +1,1 @@
-python3 run.py train --ablation True --batch_size 1 --epochs 2 --train_base_path "../../datasets/bgrem_duts_shopify_unsp_31k_sampled/train" --val_base_path "../../datasets/bgrem_duts_shopify_unsp_31k_sampled/val" --accelerator "cpu" --devices 1
+python3 run.py train --batch_size 1000 --epochs 2 --train_base_path "../../datasets/bgrem_duts_shopify_unsp_31k_sampled/train" --val_base_path "../../datasets/bgrem_duts_shopify_unsp_31k_sampled/val" --accelerator "cpu" --devices 1
